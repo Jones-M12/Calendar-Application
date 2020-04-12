18c4e8db0b709cd83568e3583c8e8dd83883d6ba
Jones-M <61522666+Jones-M12@users.noreply.github.com>
2020-04-11 20:34:38 -0400

    script.js

--- Develop/script.js
+++ Develop/script.js
@@ -13,7 +13,7 @@ $(".saveBtn").on("click", function(){
     var textContent= $(this).siblings("#textInput").val();
     var timeBlock= $(this).parent().attr("id");
     console.log(textContent);
-    localStorage.setItem("timeBlock", textContent);
+    localStorage.setItem(timeBlock, textContent);
 });
 
 //attempt to render data so it will save, does not work
