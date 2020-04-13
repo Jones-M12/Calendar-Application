0eb5eec00538bc24a57b764b33056f2886579893
Jones-M <61522666+Jones-M12@users.noreply.github.com>
2020-04-13 09:02:49 -0400

    script.js

--- script.js
+++ script.js
@@ -9,20 +9,38 @@ $("#currentDay").text(currentMoment);
 
 
 // Save button function 
-$(".saveBtn").click(function(){
+$(document).on("click",".saveBtn",function(){
     event.preventDefault();
-    var textContent= $(this).siblings("#textInput").val();
+    var textContent= $(this).siblings(".description").val();
     var timeBlock= $(this).parent().attr("id");
+    console.log(timeBlock)
     console.log(textContent);
     localStorage.setItem(timeBlock, textContent);
 });
 
 //Render Data: It works, but the last saved input overwrites the prev one. Thought that issue was solved by tartgeting the ID.
 
-$("#textInput").each(function(){
+// wroked with tutor to do troubleshoorting & resolve issue 
+
+$(".description").each(function(){
 
 var id =$(this).parent().attr("id");
 $(this).val(localStorage.getItem(id));
 
 });
 
+//to get the divs to change color
+
+//ccreate a function to add or remove classes if id is greater or less than the current time. hint: if else statements
+
+//Ari's example of how a function works
+
+// function exampleFunction(dough){
+//     console.log(dough);
+//     dough=dough+"something"
+//     console.log(dough)
+//     return dough
+// }
+
+// var usingAFunction = exampleFunction("RICE FLOUR");
+// console.log(usingAFunction)
